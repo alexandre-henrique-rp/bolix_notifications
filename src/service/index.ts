86d12e4e5d101e1082697bfc0bf6832ec88a3fae
Alexandre Henrique da Rocha Araujo <killerxandy@gmail.com>
2024-05-02 17:45:22 -0300

    update

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,7 +1,37 @@
- const NotificationService = {
-  async Boleto (token: string): Promise<any> {},
+import Notification from "../integration/notification";
 
-  async Credito (token: string): Promise<any> {},
+ const NotificationService = {
+  /**
+   * Asynchronously retrieves a Boleto payment notification using the provided token.
+   *
+   * @param {string} token - The token used to retrieve the Boleto payment notification.
+   * @return {Promise<any>} A Promise that resolves to the retrieved Boleto payment notification.
+   * @throws {Error} If an error occurs during the retrieval process.
+   */
+  async Boleto (token: string): Promise<any> {
+    try {
+      const pesquisa = await Notification(token)
+      return pesquisa
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  /**
+   * Asynchronously retrieves a Credito payment notification using the provided token.
+   *
+   * @param {string} token - The token used to retrieve the Credito payment notification.
+   * @return {Promise<any>} A Promise that resolves to the retrieved Credito payment notification.
+   * @throws {Error} If an error occurs during the retrieval process.
+   */
+  async Credito (token: string): Promise<any> {
+    try {
+      const pesquisa = await Notification(token)
+      return pesquisa
+    } catch (error) {
+      throw error;
+    }
+  },
  }
 
  export default NotificationService
